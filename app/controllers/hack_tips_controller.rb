c374c8caeecf796bdf6c5e4e1d11c8cf003883e4
Prelang Builder <builder@prelang.com>
2014-03-24 19:52:24 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/hack_tips_controller.rb
+++ app/controllers/hack_tips_controller.rb
@@ -3,7 +3,7 @@ class HackTipsController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_hack_tip, only: [:show, :edit, :update, :destroy]
+  before_action :set_hack_tip, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /hack_tips
   # GET /hack_tips.json
